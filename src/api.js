936f8cb666bb76116761c63127df9e74c1ccd307
falliso3 <falliso3@asu.edu>
2026-02-17 11:45:38 -0700

    changed API location reference in api.js

--- src/api.js
+++ src/api.js
@@ -9,7 +9,7 @@ const API_BASE =
   (typeof process !== "undefined" &&
     process.env &&
     process.env.REACT_APP_API_BASE) ||
-  "/api/fraud";
+  "/api/";
 
 /**
  * Fetch the latest fraud run summary from the backend.
